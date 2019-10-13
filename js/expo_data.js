e8dc145484559ec40df5dad4c551cf5024d2210d
Jose Manuel De Armas Camino <josemdac-vit@localhost.localdomain>
2019-10-13 17:19:22 -0400

    Changed content

--- js/expo_data.js
+++ js/expo_data.js
@@ -113,11 +113,11 @@ var slides = [{
         text: 'Explicar la solicitud de declaratoria de emergencia comprobada basada en la: REACTIVACION DE LA CONTINUIDAD OPERACIONAL DE LOS SISTEMAS DE DESTILACIÓN ATMOSFÉRICA, UNIDAD 11 Y COQUIFICACIÓN RETARDADA, UNIDAD 12 DEL MEJORADOR DE LA EMPRESA MIXTA PETRO SAN FELIX, S.A., como consecuencia de la condición operacional.',
         descripcion: 'A causa de la ruptura  de accesorios y partes internas de las Torres de Destilación Atmosférica (01C101) y Fraccionadora de Coque (01C201) se generó un incremento de presión súbita en el tren de Pre-calentamiento, ocasionando una ruptura en la línea de tubería identificada con el código P11026; la cual es una línea de desvió de los intercambiadores 01E116 A/B, lo que produjo fuga de producto masivo,  afectando todas las áreas adyacentes al tren del referido tren de pre-calentamiento.',
         items_title: 'Características para la elaboración de la declaratoria de emergencia comprobada',
-        legal: '',
+        legal: `<div style="width: 100%; margin-bottom: 1.5%">Ley de contrataciones públicas</div> <img class="art-img" src="images/caso11.svg" />`,
         doctrina: ''
     },
     template: 'caso',
-    items: [{
+    items: [/* {
         'item_text': 'Originada por circunstancias imprevistas.'
     }, {
         'item_text': 'Daños graves en el momento o futuro inmediato.'
@@ -127,18 +127,18 @@ var slides = [{
         'item_text': 'Impedir o limitar los efectos del daño.'
     }, {
         'item_text': 'Debe ser específica e individualmente para cada contratación.'
-    }]
+    } */]
 }, {
     tags: {
         title: 'Caso #2',
-        text: 'Describir las modificiaciones de contrato (equilibrio económico) en la empresa mixta Petro San Felix S.A.',
+        text: 'Las modificiaciones de contrato (equilibrio económico) en la empresa mixta Petro San Felix S.A.',
         descripcion: 'El contrato es un documento jurídico que regula la ejecución de una obra, prestación de un servicio o suministro de bienes incluidas las órdenes de compra y órdenes de servicio. Debe contener: precio, cantidades, forma de pago, tiempo, forma de entrega, condiciones, fecha de suscripción y oferta. Dichos contratos, una vez formalizados en la figura del addendum o adenda, entre los cuales recoge o contiene las modificaciones o enmiendas al contrato posterior a su firma.',
-        items_title: 'Tipos de modificaciones',
-        legal: '',
+        items_title: '',
+        legal: `<div style="width: 100%; margin-bottom: 1.5%">Ley de contrataciones públicas</div> <img class="art-img" src="images/caso21.svg" />`,
         doctrina: ''
     },
     template: 'caso',
-    items: [{
+    items: [/* {
         'item_text': 'Cambio en cantidad de la actividad contratada.'
     }, {
         'item_text': 'Cambio de alcance.'
@@ -156,7 +156,7 @@ var slides = [{
         'item_text': 'Equilibrios económicos.'
     }, {
         'item_text': 'Cierre de contrato.'
-    }]
+    } */]
 }, {
     tags: {
         title: 'final'
